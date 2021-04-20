04455391f6de335dac158beb2987a21b9994ee4c
saikat111 <s8856010@gmail.com>
2021-04-20 18:20:45 +0600

    random dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,15 +10,68 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DicePlace(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePlace extends StatefulWidget {
+  @override
+  _DicePlaceState createState() => _DicePlaceState();
+}
+
+class _DicePlaceState extends State<DicePlace> {
+  int leftDiceNumber = 1;
+  int RightDiceNumber = 1;
+  void diceNumber(){
+
+    leftDiceNumber = Random().nextInt(6);
+    RightDiceNumber = Random().nextInt(6);
+
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+
+          Expanded(
+
+              child: FlatButton(
+                onPressed: (){
+                  setState(() {
+                    diceNumber();
+
+                  });
+
+                },
+                child: (
+                    Image.asset('images/dice$leftDiceNumber.png')
+                ),
+              )),
+          Expanded(
+
+              child: FlatButton(
+                onPressed: (){
+                  setState(() {
+                    diceNumber();
+                  });
+                },
+                child: (
+                    Image.asset('images/dice$RightDiceNumber.png')
+                ),
+              )),
+        ],
+      ),
+    );
   }
 }
+
+
+
+
+
+
